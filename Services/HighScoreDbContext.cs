534ea590b54f0d973aac06f2361d627bf7e9a964
Brett Simonds <122714358+strataghast@users.noreply.github.com>
2024-04-08 20:43:40 -0400

    Added current streak, highscore streak, highscore db.

---
+++ Services/HighScoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using cadflash.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cadflash.Services
+{
+    public class HighScoreDbContext : DbContext
+    {
+        public DbSet<HighScoreStreak> HighScoreStreaks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=highscore.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HighScoreStreak>().HasData(new HighScoreStreak { Id = 1, HighestStreak = 0 });
+        }
+    }
+}
--- ViewModels/DifficultyViewModel.cs
+++ ViewModels/DifficultyViewModel.cs
@@ -4,30 +4,46 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace cadflash.ViewModels
 {
+    [QueryProperty(nameof(CurrentStreak), "CurrentStreak")]
     public partial class DifficultyViewModel : ObservableObject
     {
+        private int _currentStreak;
+        public int CurrentStreak
+        {
+            get => _currentStreak;
+            set => SetProperty(ref _currentStreak, value);
+        }
+
         [RelayCommand]
         private async Task Modify()
         {
-            await Shell.Current.GoToAsync("//crud");
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter.Add("CurrentStreak", CurrentStreak);
+            await Shell.Current.GoToAsync("//crud", parameter);
         }
 
         [RelayCommand]
         private async Task StudyEasy()
         {
-            await Shell.Current.GoToAsync("//studybasics");
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter.Add("CurrentStreak", CurrentStreak);
+            await Shell.Current.GoToAsync("//studybasics", parameter);
         }
 
         [RelayCommand]
         private async Task StudyIntermediate()
         {
-            await Shell.Current.GoToAsync("//studyintermediate");
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter.Add("CurrentStreak", CurrentStreak);
+            await Shell.Current.GoToAsync("//studyintermediate", parameter);
         }
 
         [RelayCommand]
         private async Task StudyAdvanced()
         {
-            await Shell.Current.GoToAsync("//studyadvanced");
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter.Add("CurrentStreak", CurrentStreak);
+            await Shell.Current.GoToAsync("//studyadvanced", parameter);
         }
     }
 }
